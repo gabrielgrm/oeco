88c01dcb2ce0d5b571996ada548b880dd37882e1
Kaiquegb <kaiqueghsilva@gmail.com>
2024-06-11 16:59:50 -0300

    pagina de login feita

--- renove_site/main/urls.py
+++ renove_site/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import login_view
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', login_view, name='login'),
 ]
--- renove_site/main/views.py
+++ renove_site/main/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 def index(request):
     return render(request, 'main/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        cnpj = request.POST['cnpj']
+        password = request.POST['password']
+        user = authenticate(request, username=cnpj, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or dashboard
+        else:
+            messages.error(request, 'CNPJ ou senha incorretos.')
+    form = AuthenticationForm()
+    return render(request, 'main/login.html', {'form': form})
